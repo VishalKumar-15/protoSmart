365d06d951495fe2fefac2e19fdadf4161d2bbb4
VishalKumar-15 <cherupallyvishalkumar29@gmail.com>
2025-12-07 16:00:38 +0530

    Initial commit

---
+++ src/main/java/com/app/protoSmart/ProtoSmartApplication.java
@@ -0,0 +1,13 @@
+package com.app.protoSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/protoSmart/ProtoSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.protoSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
