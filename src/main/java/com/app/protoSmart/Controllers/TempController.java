71ab53baf50899f5d745a824265729df863a4752
VishalKumar-15 <cherupallyvishalkumar29@gmail.com>
2025-12-13 11:24:27 +0530

    changes for adding the jwt authentication.

--- src/main/java/com/app/protoSmart/Controllers/TempController.java
+++ src/main/java/com/app/protoSmart/Controllers/TempController.java
@@ -1,14 +1,39 @@
 package com.app.protoSmart.Controllers;
 
+import com.app.protoSmart.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.app.protoSmart.Entities.User;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequestMapping("/api")
 public class TempController {
-    @GetMapping("/")
-    @ResponseBody
-    public String temp() {
-        return "Welcome! Your controller is working.";
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping("/users/me")
+    public ResponseEntity<User> getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User currentUser = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
 }
